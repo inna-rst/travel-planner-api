1d62f52d270ea38fbb0be1b030768ad0fff9f926
Inna <inn@gmail.com>
2026-06-05 20:57:04 +0300

    chore: add pagination, filtering and cache configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,8 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-
-    # Наши приложения
+    'django_filters',
     'planner.apps.PlannerConfig',
 ]
 
@@ -72,3 +71,19 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+}
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake",
+        "TIMEOUT": 3600,
+        "OPTIONS": {
+            "MAX_ENTRIES": 1000,
+        }
+    }
+}
